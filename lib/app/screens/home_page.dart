bfc1ef88cef06c843827d3934b53183bf47a8c81
iampato <858wpwaweru@gmail.com>
2021-05-18 15:00:06 +0300

    screen reposiveness

--- lib/app/screens/home_page.dart
+++ lib/app/screens/home_page.dart
@@ -27,8 +27,8 @@ class _HomePageState extends State<HomePage> {
               children: [
                 ConstrainedBox(
                   constraints: BoxConstraints.expand(
-                    height: MediaQuery.of(context).size.height * 0.25,
-                    width: MediaQuery.of(context).size.width * 0.35,
+                    height: MediaQuery.of(context).size.height * 0.22,
+                    width: MediaQuery.of(context).size.width * 0.32,
                   ),
                   child: ClipRRect(
                     borderRadius: BorderRadius.only(
