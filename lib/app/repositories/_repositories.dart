bcb3d9c8b3188c15a6b4c8ef2f0ecc548f5d6b23
iampato <858wpwaweru@gmail.com>
2021-05-19 20:54:51 +0300

    added the http repo layer

---
+++ lib/app/repositories/_repositories.dart
@@ -0,0 +1,11 @@
+library repository;
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:qhala/app/utils/exceptions.dart';
+import 'package:qhala/app/utils/http_client.dart';
+import 'package:qhala/app/utils/qhala_configs.dart';
+
+part 'network_repo.dart';
+part 'db_repo.dart';
--- lib/app/repositories/network_repo.dart
+++ lib/app/repositories/network_repo.dart
@@ -1,1 +1,40 @@
+part of '_repositories.dart';
 
+class MovieRepository {
+  // Params
+  HttpNetworkUtil _networkUtil;
+  // Setup a singleton
+  static final MovieRepository _movieRepository = MovieRepository._internal();
+  factory MovieRepository({HttpNetworkUtil networkUtil}) {
+    _movieRepository._networkUtil = networkUtil;
+    return _movieRepository;
+  }
+  MovieRepository._internal();
+  // Global Variables
+  Logger _logger = Logger();
+
+  /// httpGetPopularMovies, endpoint is 
+  /// `https://api.themoviedb.org/3/movie/popular?api_key=YOUR_KEY&language=en-US&page=1`
+  /// params -> the page number since the API is paginated
+  /// return Future Movie model
+  /// if an error occurres return null
+  Future<String> httpGetPopularMovies({
+    @required int page,
+  }) async {
+    _logger.i("Page number: $page");
+    Response response = await _networkUtil.getRequest(
+      "/popular?api_key=$API_KEY&language=en-US&page=$page",
+    );
+    if (response != null) {
+      if (response.statusCode == 404) {
+        throw InternetException();
+      } else if (response.statusCode == 200) {
+        return response.data.toString();
+      } else {
+        return response.data.toString();
+      }
+    } else {
+      return null;
+    }
+  }
+}
