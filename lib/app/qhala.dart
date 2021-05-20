0c485baeba76c739a82ac5bcd64047ff96738b3b
iampato <858wpwaweru@gmail.com>
2021-05-20 10:11:29 +0300

    added a dark theme

--- lib/app/qhala.dart
+++ lib/app/qhala.dart
@@ -3,16 +3,28 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:qhala/app/screens/home_page.dart';
 import 'package:qhala/app/widgets/color_pallete.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Movie App',
       theme: ThemeData(
         primarySwatch: Colors.red,
+        brightness: Brightness.light,
         fontFamily: GoogleFonts.openSans().fontFamily,
         scaffoldBackgroundColor: AppTheme.theme(context).bg1,
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: Brightness.dark,
+        fontFamily: GoogleFonts.openSans().fontFamily,
+        scaffoldBackgroundColor: Colors.black,
       ),
       home: HomePage(),
     );
