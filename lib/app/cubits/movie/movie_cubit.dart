3d23fdd07fcc055b81c3965734485e0164994b76
iampato <858wpwaweru@gmail.com>
2021-06-04 14:03:27 +0300

    moving from cubit to hydrated bloc

--- lib/app/cubits/movie/movie_cubit.dart
+++ lib/app/cubits/movie/movie_cubit.dart
@@ -1,13 +1,15 @@
-import 'package:bloc/bloc.dart';
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:qhala/app/models/http/movie_model.dart';
 import 'package:qhala/app/repositories/_repositories.dart';
 import 'package:qhala/app/utils/exceptions.dart';
 
 part 'movie_state.dart';
 
-class MovieCubit extends Cubit<MovieState> {
+class MovieCubit extends HydratedCubit<MovieState> {
   final MovieRepository movieRepository;
   MovieCubit({@required this.movieRepository}) : super(MovieInitial());
 
@@ -94,5 +96,32 @@ class MovieCubit extends Cubit<MovieState> {
         }
       }
     }
+  }
+
+  @override
+  MovieState fromJson(Map<String, dynamic> json) {
+    bool fetchMore = json["fetch_more"];
+    String message = json["message"];
+    String movieString = json["movie"];
+    final movieJson = jsonDecode(movieString);
+
+    return MovieLoaded(
+      doneFetchingMore: fetchMore,
+      message: message,
+      movieModel: MovieModel.fromJson(movieJson),
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson(MovieState state) {
+    final currentState = state;
+    if (currentState is MovieLoaded) {
+      return {
+        "fetch_more": currentState.doneFetchingMore,
+        "message": currentState.message,
+        "movie": currentState.movieModel.toJson(),
+      };
+    }
+    return null;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:qhala/app/cubits/Theme/theme_cubit.dart';
 import 'package:qhala/app/repositories/_repositories.dart';
 import 'package:qhala/app/utils/cubit_observer.dart';
@@ -10,6 +12,9 @@ import 'app/qhala.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
   runApp(
     BlocProvider(
       create: (context) {
